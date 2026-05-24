9679ef647671da572c40259832bdc0362df2556e
amtulsaboor <saboooorrrrrr@gmail.com>
2026-05-24 16:45:29 +0530

    update

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Ultimate DevSecOps CI/CD Pipeline Running Successfully")
+    return HttpResponse("Ultimate DevSecOps CI/CD Pipeline Running Successfully! Well Done Miss SABOOR")
 
 # Create your views here.
